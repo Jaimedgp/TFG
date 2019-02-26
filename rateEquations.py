5fa2c0cc00d5dbd785384f575d264baa12f65b5a
Jaime <jaime.diez.gp@gmail.com>
2019-02-26 16:42:46 +0100

    rate equations simulation without noise and inyection

---
+++ rateEquations.py
@@ -0,0 +1,10 @@
+"""
+    Simulation of the rate equations used to discribe the dynamics of the
+    carrier density (N(t)), the photon density (S(t)) and optical phase (phi(t))
+
+    The simulation is done without taking into account the noise and the
+    inyection terms
+
+    author: JaimeDGP
+    latest version: 26 February 2019
+"""
