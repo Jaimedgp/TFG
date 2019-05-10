19f0d22296913ea864aba7c610ffdde6dc1a845b
Jaime <jaime.diez.gp@gmail.com>
2019-05-10 13:22:34 +0200

    Change the graph

--- src/rateEquations.py
+++ src/rateEquations.py
@@ -9,6 +9,10 @@
 """
 
 import numpy as np
+
+from matplotlib import rcParams
+rcParams['font.family'] = 'serif'
+
 import matplotlib.pyplot as plt
 import cmath
 
@@ -18,6 +22,10 @@ from getTempValues import getDeltaT
 iBias = 30  # bias current [mA] / must be in [C ns^-1] by multiplying *10**-12
 vRF = [0.05 *10**(-9), 1 *10**(-9), 1.5 * 10**(-9)] #RMS voltage value of the signal generator [V]
 colors = ['g', 'b', 'r']
+graphLabel = [['(a)', '(b)', '(c)'],
+              ['(d)', '(e)', '(f)'],
+              ['(g)', '(h)', '(i)'],
+              ['(j)', '(k)', '(l)']]
 
 deltaT = getDeltaT(iBias)
 
@@ -133,23 +141,34 @@ for i in range(len(vRF)):
     ##  Representacion de los Datos
     #########################################
 
-    axs[0][i].set_title("$V_{RF} = $%.2f V" %(vRF[i] * 10**9), color=colors[i], fontsize=15)
+    axs[0][i].set_title("$V_{RF} = $%.2f V" %(vRF[i] * 10**9), color=colors[i],
+                                                                    fontsize=15)
 
     axs[0][i].plot(timePeriod, I, colors[i])
     axs[0][i].axhline(y=14.8, linestyle=":", color='k', linewidth=3)
     axs[0][i].grid(linestyle='-.')
+    axs[0][i].annotate(graphLabel[0][i], (0.9, 0.85),
+                                            xycoords='axes fraction', size=20)
+    #axs[0][i].text(0.25, 0.75, graphLabel[0][i])
 
     axs[1][i].plot(timePeriod, S, colors[i])
     axs[1][i].grid(linestyle='-.')
+    axs[1][i].annotate(graphLabel[1][i], (0.9, 0.85),
+                                            xycoords='axes fraction', size=20)
 
     axs[2][i].plot(timePeriod, dPhi, colors[i], label="N(t)")
     axs[2][i].grid(linestyle='-.')
+    axs[2][i].annotate(graphLabel[2][i], (0.9, 0.85),
+                                            xycoords='axes fraction', size=20)
+    #axs[2][i].set_ylim([-75, 50])
 
     axs[3][i].plot(timePeriod, N/nTr, colors[i])
     axs[3][i].grid(linestyle='-.')
+    axs[3][i].annotate(graphLabel[3][i], (0.9, 0.85),
+                                            xycoords='axes fraction', size=20)
 
 axs[0][0].set_ylabel("I(t) [$mA$]", fontsize=15)
-axs[1][0].set_ylabel("S(t) [$m^3$]", fontsize=15)
+axs[1][0].set_ylabel("S(t) [$m^{-3}$]", fontsize=15)
 axs[2][0].set_ylabel("Chirp [GHz]", fontsize=15)
 axs[3][0].set_ylabel("$N(t) / N_{Tr}$", fontsize=15)
 
