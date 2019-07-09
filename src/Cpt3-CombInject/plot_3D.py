d5e59b3d9469f673be7f630f9d340610f47bcc75
Jaimedgp <jaime.diez.gp@gmail.com>
2019-07-09 17:00:22 +0200

    minor updates

--- src/Cpt3-CombInject/plot_3D.py
+++ src/Cpt3-CombInject/plot_3D.py
@@ -3,22 +3,6 @@
         of the semiconductor laser in sinusoidal wave (SW) with optical
         injection cahracterized by power injection (pwrInjct) and the detuning
         respect of the emission frequency (nuDetng)
-
-        The squeme used to plot the data is the following:
-
-                            +-------------------------+
-                            |                         |
-                            |                         |
-                            |          PSD            |
-                            |                         |
-                            |                         |
-                            +-------------------------+
-
-                +--------------+  +--------------+  +--------------+
-                |              |  |              |  |              |
-                |  power / N   |  |   N / time   |  | Phase / time |
-                |              |  |              |  |              |
-                +--------------+  +--------------+  +--------------+
 """
 
 __author__ = 'Jaime Diez G-P'
@@ -29,6 +13,7 @@ __date__ = "Jun 26, 2019"
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import os.path
 
 import sys
@@ -64,7 +49,7 @@ fR = 5.0 # [GHz]
 #   Master laser
 #----------------------------
 
-pwrInjct = 20000
+pwrInjct = 100
 # detuning of the injected laser field with respect to the emission frequency
 nuDetng = -2 # [GHz]
 
@@ -125,66 +110,15 @@ else:
 #--------------------------------------------
 #  Takes the values of the study inteval 
 #--------------------------------------------
-
+"""
 indexes = np.where((time > transient) & (time < period+transient))
 time = time[indexes]
-power = constP * S[indexes] *10**(12)
 N = N[indexes]
 Phi = Phi[indexes] - 2*np.pi*(nuDetng - f0 + (c0/emissnWL[iBias]))*time
+"""
+power = constP * S *10**(12)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-########################################
-##          PLOT DATA                 ##
-######################################## 
-
-gridsize = (3, 6)
-fig = plt.figure(figsize=(20, 10))
-ax1 = plt.subplot2grid(gridsize, (0, 1), colspan=4, rowspan=2)
-
-ax1.plot(fftWL, TFavg)
-ax1.set_yscale("log")
-ax1.set_xlabel("$\lambda$ [nm]")
-ax1.set_ylabel("PSD")
-
-ax2 = plt.subplot2grid(gridsize, (2, 0), colspan=2, rowspan=1)
-ax2.plot(N, power)
-ax2.set_xlabel("$N(t) / N_{Tr}$", fontsize=15)
-ax2.set_ylabel("Power [mW]", fontsize=15)
-
-ax3 = plt.subplot2grid(gridsize, (2, 2), colspan=2, rowspan=1)
-ax3.plot(time, power)
-ax3.set_xlabel("t [ns]", fontsize=15)
-ax3.set_ylabel("$Power [mW]$", fontsize=15)
-
-ax4 = plt.subplot2grid(gridsize, (2, 4), colspan=2, rowspan=1)
-ax4.plot(time, Phi)
-ax4.set_xlabel("t [ns]", fontsize=15)
-ax4.set_ylabel("\Phi", fontsize=15)
-
-title = ("Inyeccion de Luz con $P_{inj} = $ %s" %(pwrInjct)
-         +"$\mu$W y detuning $\delta \\nu = $%s GHz" %(nuDetng)
-        )
-ax1.set_title(title, fontsize=25)
-
-#-----------------------------------------------
-#  Mark with an arrow the injection wavelength
-#-----------------------------------------------
-
-# Injection wavelength
-wLInject = (c0*10**(9))/(c0/emissnWL[iBias] + nuDetng)
-
-indexes = np.argmax(np.where((fftWL > wLInject)))
-yValue = max(TFavg[indexes-5:indexes+5])
-arrwPost = 40 * yValue
-arrwLngth = yValue-arrwPost
-arrwHdLngth = 3.5 * yValue
-
-ax1.arrow(x=wLInject, y=arrwPost,
-          dx=0, dy=arrwLngth,
-          length_includes_head=True,
-          head_width=0.01,
-          head_length=arrwHdLngth,
-          color='r'
-         )
-
-plt.tight_layout()
+ax.plot(N, power, Phi)
 plt.show()
